633e83683f37668e3b8f73094d8f9385d2bb7030
Mohamed Abdelfattah <mhabdelfattah22@gmail.com>
2024-07-10 00:19:30 +0300

    simple caching

--- fetch_problemset.py
+++ fetch_problemset.py
@@ -6,20 +6,14 @@ import sqlite3
 def fetch():
     url = "https://codeforces.com/api/problemset.problems"
     prblmset = requests.get(url)
-
     problemset = json.loads(prblmset.text)
 
     problems = problemset['result']['problems']
-
-    df = pd.DataFrame(problems).drop(['points','type'],axis=1)
-
-    def convert(tags):
-        return ', '.join(tags)
-
-    df['tags'] = df['tags'].apply(lambda x: convert(x))
+    df = pd.DataFrame(problems).drop(['points', 'type'], axis=1)
+    df['tags'] = df['tags'].apply(lambda x: ', '.join(x))
 
     conn = sqlite3.connect("problemset.db")
-
     df.to_sql('problems', conn, if_exists='replace', index=False)
-
+    conn.execute("CREATE INDEX IF NOT EXISTS idx_rating ON problems (rating)")
+    
     conn.close()
