b377fb0c3c7d8b4c8a7cbcbec465b137dbd5d725
Mohamed Abdelfattah <mhabdelfattah22@gmail.com>
2024-07-10 16:18:31 +0300

    changed to work without db

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, get_flashed_messages
 from flask_caching import Cache
 import pandas as pd
-import sqlite3
 import requests
 from fetch_problemset import fetch
 from handles import merge_solved_problems
@@ -9,35 +8,17 @@ from handles import merge_solved_problems
 app = Flask(__name__)
 app.secret_key = 'e9aa5b78bce79caa06c445b364058979840b1ab60338fdc6'
 
-app.config['data_fetched'] = False
 app.config['CACHE_TYPE'] = 'simple'
 cache = Cache(app)
 
-@app.before_request
-def initialize_data():  
-    if not app.config['data_fetched']:
-        try:
-            fetch()
-            app.config['data_fetched'] = True
-            print("Data Fetched")
-        except (requests.ConnectionError, requests.HTTPError) as e:
-            print("Failed to fetch data:", e)
-    else:
-        print("Data Already Fetched")
-
 @cache.memoize(timeout=300)
-def retrieve_data(min_range, max_range, problems_num):
-    conn = sqlite3.connect("problemset.db")
-    query = f"SELECT * FROM problems WHERE rating >= {min_range} AND rating <= {max_range}"
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df.sample(problems_num)
+def retrieve_data(min_range, max_range):
+    df = fetch()
+    filtered_df = df[(df['rating'] >= min_range) & (df['rating'] <= max_range)]
+    return filtered_df
 
 def get_codes(frame):
-    codes = []
-    for i in range(len(frame)):
-        code = f"{int(frame['contestId'].iloc[i])}{frame['index'].iloc[i]}"
-        codes.append(code)
+    codes = [f"{int(frame['contestId'].iloc[i])}{frame['index'].iloc[i]}" for i in range(len(frame))]
     return ', '.join(codes)
 
 @app.route('/')
@@ -47,15 +28,13 @@ def index():
 
 @cache.memoize(timeout=300)
 def get_unsolved_problems(handles, min_range, max_range):
-    conn = sqlite3.connect("problemset.db")
-    query = f"SELECT * FROM problems WHERE rating >= {min_range} AND rating <= {max_range}"
-    all_problems = pd.read_sql(query, conn)
-    conn.close()
-
+    all_problems = fetch()
+    filtered_problems = all_problems[(all_problems['rating'] >= min_range) & (all_problems['rating'] <= max_range)]
+    
     solved_problems = merge_solved_problems(handles)
     solved_problems_codes = solved_problems.apply(lambda row: f"{int(row['contestId'])}{row['index']}", axis=1)
-
-    unsolved_problems = all_problems[~all_problems.apply(lambda row: f"{int(row['contestId'])}{row['index']}", axis=1).isin(solved_problems_codes)]
+    
+    unsolved_problems = filtered_problems[~filtered_problems.apply(lambda row: f"{int(row['contestId'])}{row['index']}", axis=1).isin(solved_problems_codes)]
     
     return unsolved_problems
 
@@ -75,17 +54,18 @@ def result():
         handles = [request.form.get('individual_handle')]
     else:
         handles = [request.form.get(f'team_handle_{i}') for i in range(1, 4) if request.form.get(f'team_handle_{i}')]
-    
+
     print(f"\n{'='*10} {len(handles)} {'='*10}\n")
     print(f"\n{'='*10} {handles} {'='*10}\n")
 
-    if (len(handles) == 1 and handles[0] == '' ) or (len(handles) == 0) :
+    if len(handles) == 0 or (len(handles) == 1 and handles[0] == ''):
         print("No Handle")
-        df = retrieve_data(min_range, max_range, problems_num)
+        df = retrieve_data(min_range, max_range).sample(problems_num)
     else:
         print("Using Handle")
         unsolved_df = get_unsolved_problems(handles, min_range, max_range)
         df = unsolved_df.sample(problems_num)
+
     codes = get_codes(df)
 
     return render_template('result.html', codes=codes)
--- fetch_problemset.py
+++ fetch_problemset.py
@@ -12,8 +12,4 @@ def fetch():
     df = pd.DataFrame(problems).drop(['points', 'type'], axis=1)
     df['tags'] = df['tags'].apply(lambda x: ', '.join(x))
 
-    conn = sqlite3.connect("problemset.db")
-    df.to_sql('problems', conn, if_exists='replace', index=False)
-    conn.execute("CREATE INDEX IF NOT EXISTS idx_rating ON problems (rating)")
-    
-    conn.close()
+    return df
